a193fa7bac219f7a2b1923a7bf149fb87ff6ef9b
andrewnnov <andrewnnov@yandex.ru>
2023-08-22 20:21:04 -0300

    lesson4

---
+++ src/main/java/lesson4/BankAccount.java
@@ -0,0 +1,18 @@
+package lesson4;
+
+public class BankAccount {
+
+    int id;
+    String name;
+    double balance;
+
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount();
+        BankAccount ma = new BankAccount();
+        BankAccount ha = new BankAccount();
+        ba.id = 1;
+        ba.name = "Name";
+        ba.balance = 12.35;
+
+    }
+}
---
+++ src/main/java/lesson4/Test.java
@@ -0,0 +1,25 @@
+package lesson4;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
+
+
+class TestPlus {
+    public static void main(String[] args) {
+        System.out.println("TestPlus");
+    }
+
+}
+
+class TestPlusOne {
+
+    public static void main(String[] args) {
+        System.out.println("TestPlusOne");
+    }
+
+
+}
